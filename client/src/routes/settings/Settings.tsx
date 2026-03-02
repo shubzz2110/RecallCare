e274861198a6755e682460dbe922af308dc2aefc
Shubham Homkar <homkar1997@gmail.com>
2026-03-02 16:17:13 +0530

    Restart

--- client/src/routes/settings/Settings.tsx
+++
@@ -1,9 +0,0 @@
-import { setTitle } from "@/lib/set-title";
-import { useEffect } from "react";
-
-export default function Settings() {
-  useEffect(() => {
-    setTitle("Settings");
-  }, []);
-  return <div>Settings</div>;
-}
--- server/src/validations/appointment.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const createAppointmentSchema = z.object({
-  patientId: z.string(),
-  scheduledDate: z.string(),
-});
